3ecb84e87bd32bc38ac7b0734a1cf29e76848c79
Heemin <viridian135@gmail.com>
2022-08-07 14:32:08 +0900

    day10 redux and toolkit

--- day10_redux/src/store/modules/color.js
+++ day10_redux/src/store/modules/color.js
@@ -1,27 +1,26 @@
-//const AAA = 'filename/AAA'
-const RED = "color/RED";
-const GREEN = "color/GREEN";
-const BLUE = "color/BLUE";
+//const AAA = '파일명/AAA';
+const RED  = 'color/RED'
+const GREEN  = 'color/GREEN'
+const BLUE  = 'color/BLUE'
 
-//액션 내보내기
-export const red = () => ({ type: RED });
-export const green = () => ({ type: GREEN });
-export const blue = () => ({ type: BLUE });
+//액션내보내기 
+export const red = ()  => ({type:RED})
+export const green = ()  => ({type:GREEN})
+export const blue = ()  => ({type:BLUE})
 
-//리듀서 초기값
-const initialState = {};
-//reducer 순수함수
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case RED:
-      return { color: "red" };
-    case GREEN:
-      return { color: "green" };
-    case BLUE:
-      return { color: "blue" };
-    default:
-      return state;
-  }
-};
-
-export default reducer;
+//리듀서 초기값 
+const initialState  = { color: 'hotpink'}
+//리듀서 순수함수 
+const reducer  = (state = initialState, action)  => {
+    switch( action.type ) {
+        case RED:
+            return { color:'red'}
+        case GREEN:
+            return { color:'green'}
+        case BLUE:
+            return { color:'blue'}
+        default:
+            return state 
+    }
+}
+export default reducer 
--- day10_toolkit/src/index.js
+++ day10_toolkit/src/index.js
@@ -3,17 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux'; 
-import rootReducer  from './store'
-
-const store  = createStore( rootReducer  )
+import store from './store'
+import { Provider } from 'react-redux' 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
+  <React.StrictMode>     
+    <Provider store={store} >
+      <App />    
     </Provider>
   </React.StrictMode>
 );
